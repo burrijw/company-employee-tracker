64e89d5b3fe43c59cf104781b764ff96c86f33e0
hannahpsmith <166345156+hannahpsmith@users.noreply.github.com>
2024-08-20 19:34:10 -0600

    updated seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE tracker_db;
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL,
-)
+);
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
-)
+);
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
@@ -20,4 +20,4 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name) VALUES 
+('Marketing'),
+('Finance'),
+('Information Technology');
+
+INSERT INTO role (title, salary, department_id) VALUES 
+('Manager', 70000, 1),
+('Accountant', 60000, 1),
+('IT Support Technician', 70000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Bob', 'Smith', 1, 1); 
